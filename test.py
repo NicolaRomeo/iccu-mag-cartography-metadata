eaa5293da19078bdeb2fb78e932ce2770d9ef7b1
nromeo <nicolaromeo1@gmail.com>
2022-01-03 18:43:45 +0100

    saving all images metadata to a json file (test.py)

--- test.py
+++ test.py
@@ -13,7 +13,9 @@ import zipfile
 import subprocess
 import filetype
 import shutil
-from exif import Image
+from PIL import Image as Image_PIL
+import hashlib
+from exif import Image as IMAGE_exif
 
 
 class ExifTool(object):
@@ -51,7 +53,6 @@ def carica_foto(*args):
     def extract_zip(input_zip):
         input_zip = zipfile.ZipFile(input_zip)
         return {name: input_zip.read(name) for name in input_zip.namelist()}
-    print('sono dentro la funzione carica_foto')
     Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
     filename = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
     print(filename)
@@ -74,30 +75,27 @@ def carica_foto(*args):
     p.mkdir(parents=True, exist_ok=True)
     with zipfile.ZipFile(filename, 'r') as zip_ref:
         zip_ref.extractall(p.resolve())
-    print('p.resolve = {0}'.format(p.resolve()))
     dest_folder = filename.split('/')[-1].removesuffix('.zip')
     full_path_to_images = Path(base_path / dest_folder)
-    print('full path to images {}'.format(full_path_to_images))
     filenames = []
     for imagename in os.listdir(full_path_to_images):
-        print('sono dentro la directory')
-        print(imagename)
         image_path = full_path_to_images / imagename
         full_path = image_path.absolute()
         my_path = full_path.as_posix()
-        print('image path {}'.format(my_path))
         if filetype.is_image(my_path):
-            print("il file {} è un'immagine".format(my_path))
             filenames.append(my_path)
         else:
             continue
-    print('filenames is {}'.format(filenames))
+    print('elenco delle immagini recuperate: {}'.format(filenames))
+
 
     #EXTRACT METADATA USING EXIFTOOLS (Working as of 2021-12-29)
     with ExifTool() as e:
-        metadata = e.get_metadata(*filenames)
+        exiftools_metadata = e.get_metadata(*filenames)
+        '''
         print('metadata is a {}'.format(type(metadata)))
         print(metadata)
+        '''
         e.__exit__('i','j','k')
 
     #starts writing the xml file in append mode, because img is the last of the tags.
@@ -106,46 +104,57 @@ def carica_foto(*args):
     #process of generating the rest of the input
 
 
+    # USE PILLOW (PIL) TO GET IMAGE METADATA
+    Pillow_img_metadata = {}
+    for im in filenames:
+        image = Image_PIL.open(im)
+        md5hash = hashlib.md5(Image_PIL.open(im).tobytes())
+        '''
+        print(
+            "filename: {} \n".format(image.filename),
+            "mode: {} \n".format(image.mode), "size: {} \n".format(image.size), "format: {} \n".format(image.format),
+            "info: {} \n".format(image.info),
+            "md5: {} \n".format(md5hash)
+            "********************************\n"
+        )
+        '''
+        Pillow_img_metadata[image.filename]= {"name":image.filename,"mode":image.mode,"size":image.size,"format":image.format,"md5":md5hash.hexdigest(),"info":image.info}
 
-    #bisogna fare una traduzione tra il significato in exiftool e nello standard NISO
-
-'''
-    MAG/NISO:
-    - 1 : nessuna unità di misura definita
-    - 2 : inch, pollice
-    - 3 : centimetro
-    EXIFTOOL:
-    0 = None
-    1 = inches
-    2 = cm
-    
-    
-    
-'''
-'''
-    for image_data in metadata:
-    
-    #use hachoir python command tool to gather images metadata
-    infoDict = {}
-    for img in filenames:
-        exeProcess = "hachoir-metadata"
-        process = subprocess.Popen([exeProcess, img],
-                                   stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-                                   universal_newlines=True)
-
-        for tag in process.stdout:
-            line = tag.strip().split(':')
-            infoDict[line[0].strip()] = line[-1].strip()
-
-        for k, v in infoDict.items():
-            print(k, ':', v)
+    #we create all the xml tags with immutable general information that are required in the <img> tag before adding actual metadata
+    #this information contains: sequence_number,nomenclature, usage, side, file location
+    counter = 1
+    immutable_metadata= {}
+    for im in filenames:
+        sequence_number = counter
+        nomenclature = "Pagina {}".format(counter)
+        #we will not implement the usage and side tags in this version of the application
+        file_location = im.split('/')[-1]
+        '''
+        print('image: {} \n'
+              'sequence_number: {} \n'
+              'nomenclature: {} \n'
+              'file_location: {} \n'.format(im,sequence_number,nomenclature,file_location))
+        '''
+        counter = counter + 1
+        immutable_metadata[file_location]= {'image':im}
+        immutable_metadata[file_location].update({'sequence_number':sequence_number})
+        immutable_metadata[file_location].update({'nomenclature': nomenclature})
+    #print("immutable_metadata {}".format(immutable_metadata))
+    #USE EXAMPLE OF EXIFTOOL to get complete exif metadata of the image
     '''
+    for file in metadata:
+        print(file.keys())
+        print("getting file name from exiftool metadata: {}".format(file["File:FileName"]))
+    '''
+    #create a dictionary with all available metadata and save to a json file
+    complete_metadata = [immutable_metadata,Pillow_img_metadata,exiftools_metadata]
+    metadata_json_file = json.dumps(complete_metadata)
+    print(metadata_json_file)
+    destination = base_path / "data.json"
+    with open(destination, "w") as jsonFile:
+        jsonFile.write(metadata_json_file)
 
 
-
-    #estrai lo zip in memoria
-    #lista_immagini = extract_zip(filename)
-    #print(lista_immagini)
 
 
 def run_app():
